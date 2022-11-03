9665c34976cc7031b3670be5c1d1ff836c16abd2
rishabh3201 <100549788+rishabh3201@users.noreply.github.com>
2022-11-03 17:47:24 +0530

    Add files via upload

---
+++ responses.js
@@ -0,0 +1,10 @@
+function getBotResponse(input){
+	if(input=="hello"){
+		return "hello there!";
+	} else if(input=="goodbye"){
+		return "Talk to you later!";
+	} else{
+		return "try asking something else!";
+	}
+
+}
